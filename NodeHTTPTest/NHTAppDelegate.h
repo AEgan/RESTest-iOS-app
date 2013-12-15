77e669ab924b4e61a5258e5c7f30855e5b5c9aa9
Alex Egan <aegan@andrew.cmu.edu>
2013-12-15 00:42:51 -0500

    Initial Commit

---
+++ NodeHTTPTest/NHTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NHTAppDelegate.h
+//  NodeHTTPTest
+//
+//  Created by Alex Egan on 12/15/13.
+//  Copyright (c) 2013 AEgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NHTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NodeHTTPTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NodeHTTPTest
+//
+//  Created by Alex Egan on 12/15/13.
+//  Copyright (c) 2013 AEgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NHTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NHTAppDelegate class]));
+    }
+}
