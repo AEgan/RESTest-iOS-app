71acbcaa1fe6df6977d02d6d8fd3754b0d1bc4f0
Alex Egan <aegan@andrew.cmu.edu>
2014-01-03 23:56:34 -0500

    :octocat: edit controller resigns keyboard on outside tap

--- NodeHTTPTest/NHTEditViewController.m
+++ NodeHTTPTest/NHTEditViewController.m
@@ -27,6 +27,8 @@
 {
     [super viewDidLoad];
     self.title = @"Editing Message";
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:tap];
     [self.authorField setText:[self.message valueForKey:@"author"]];
     [self.messageField setText:[self.message valueForKey:@"message"]];
     // Do any additional setup after loading the view from its nib.
@@ -38,6 +40,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)dismissKeyboard {
+    [self.messageField resignFirstResponder];
+    [self.authorField resignFirstResponder];
+}
+
 - (IBAction)submitPressed:(id)sender {
+//    NSString *authorString = [self.authorField text];
+//    NSString *messageString = [self.messageField text];
+//    NSString *fixedStr = [messageString stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
+//    NSString *urlStr = [NSString stringWithFormat:@"http://localhost:12345?author=%@&message=%@", authorString, fixedStr];
+//    NSURL *url = [NSURL URLWithString:urlStr];
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+//    [request setHTTPMethod:@"POST"];
+//    NSError *err1;
+//    NSData *returnedData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&err1];
+//    if(err1){
+//        UIAlertView *connectionAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Unable to connect to API. Check that the server is running" delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+//        [connectionAlert show];
+//    }
+//    else {
+//        [self.navigationController popViewControllerAnimated:YES];
+//    }
 }
 @end
